378d7729af38b3699ed1ebecfb234de801495065
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-10-27 17:50:21 -0400

    response object

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content-type
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<p>hello world</p>');
+  res.write('<p>hello again</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
